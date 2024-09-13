86ad67c906f67cda4adc9d9003ac72bbfc6384c6
Lucas Correia <lucasdevproject@gmail.com>
2024-09-13 13:02:41 -0300

    Functions

---
+++ loopings/Continue_Break.php
@@ -0,0 +1,18 @@
+<?php 
+$nome= ['Lucas', 'Maria', 'joão'];
+
+for($i=0; $i < count($nome); $i++){
+    if($i === 1){
+        continue;
+    } if($i ===2){
+        break;
+    }
+        
+    
+    
+   }
+
+  // continue: Pula o restante do código da iteração atual e vai para a próxima iteração do loop.
+  // break: Sai imediatamente do loop, terminando a execução do loop.
+
+?>
---
+++ loopings/Foreach.php
@@ -0,0 +1,12 @@
+<?php 
+$nome= ['Lucas', 'Maria', 'joão'];
+
+
+//Para cada
+foreach($nome as $key => $nome){
+    //Para cada valor guardado no array e guardado na $key = que é o indice e $nome = que é o valor 
+}
+
+
+
+?>
